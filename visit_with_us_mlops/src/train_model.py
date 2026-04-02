3d1529ec8d192dfe40ccd8daa94fc832af7658f9
gaurav srivastava <g.sri2483@gmail.com>
2026-04-02 18:15:36 +0000

    fixing files strucutre

--- visit_with_us_mlops/src/train_model.py
+++
@@ -1,7 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-import joblib
-
-model = RandomForestClassifier(random_state=RANDOM_STATE)
-model.fit(X_train, y_train)
-
-joblib.dump(model, 'visit_with_us_mlops/model/random_forest_model.pkl')
